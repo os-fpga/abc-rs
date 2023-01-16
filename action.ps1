58583799b771ea4729825ff477afee07bf0256cc
Nairi-RS <nairi.baveyan@rapidsilicon.com>
2023-01-16 19:14:03 +0400

    action.ps1 changed

--- action.ps1
+++ action.ps1
@@ -1,21 +1,33 @@
 
 Write-Output "abc action script start "
 
+$OldPATH = $PATH
+$env:PATH = (Test-Path -Path "C:\cygwin64\bin") ? "C:\cygwin64\bin\" : "C:\cygwin\bin\"
+$env:PATH -split ";"
+$Cygwin = $env:PATH + "bash.exe"
+$arg = "-c"
 
-sed -i 's#ABC_USE_PTHREADS\"#ABC_DONT_USE_PTHREADS\" /D \"_ALLOW_KEYWORD_MACROS=1\"#g' .\*.dsp
-awk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' .\abclib.dsp > .\tmp.dsp
+& $Cygwin $arg "sed -i 's#ABC_USE_PTHREADS\"#ABC_DONT_USE_PTHREADS\" /D \"_ALLOW_KEYWORD_MACROS=1\"#g' .\*.dsp"
+& $Cygwin $arg "gawk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' .\abclib.dsp > .\tmp.dsp"
+& $Cygwin $arg "unix2dos .\*.dsp"
+$env:PATH = $OldPath
+
 copy .\tmp.dsp .\abclib.dsp
 del .\tmp.dsp
 unix2dos .\*.dsp
 <# executing MsDevShell #>
-pwsh.exe .\function.ps1
+./function.ps1
 
 devenv abcspace.dsw /upgrade  ; if (-not $? ) { cat UpgradeLog.htm }
 msbuild abcspace.sln /m /nologo /p:Configuration=Release /p:PlatformTarget=x86
 
 <# After build we copying abc.exe, abc.rc and pthreadVC2.dll files to Release directory #>
+$CurLoc = Get-Location
+Write-Output $CurLoc
+mkdir ..\..\x64
+mkdir ..\..\x64\Release
 copy .\lib\x64\pthreadVC2.dll ..\..\x64\Release
 copy .\_TEST\abc.exe ..\..\x64\Release
 copy .\abc.rc ..\..\x64\Release
 
-Write-Outpu "abc action script end"
+Write-Output "abc action script end"
