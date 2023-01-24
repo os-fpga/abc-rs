9c8e743008b4640146ed30f27df8bfcb63f04995
unknown <nairi.baveyan@rapidsilicon.com>
2023-01-24 18:56:43 +0400

    Use Winutils instead of cygwin

--- action.ps1
+++ action.ps1
@@ -4,28 +4,22 @@ Write-Output "abc action script starts "
 Write-Output "Path: $env:PATH"
 
 $OldPATH = $env:PATH
-$env:PATH = (Test-Path -Path "C:\cygwin64\bin") ? "C:\cygwin64\bin\" : "C:\cygwin\bin\"
+<#$env:PATH = (Test-Path -Path "C:\cygwin64\bin") ? "C:\cygwin64\bin\" : "C:\cygwin\bin\"
 $env:PATH -split ";"
 $CygwinBin = $env:PATH
 $Cygwin = $env:PATH + "bash.exe"
-$arg = "-c"
+$arg = "-c"#>
 
-& $Cygwin $arg "sed -i 's#ABC_USE_PTHREADS\"#ABC_DONT_USE_PTHREADS\" /D \"_ALLOW_KEYWORD_MACROS=1\"#g' .\*.dsp"
+sed -i 's#ABC_USE_PTHREADS\"#ABC_DONT_USE_PTHREADS\" /D \"_ALLOW_KEYWORD_MACROS=1\"#g' .\*.dsp
 gawk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' .\abclib.dsp > .\tmp.dsp
-& $Cygwin $arg "unix2dos .\*.dsp"
-$env:PATH = $OldPath
-
 
 Write-Output "Path: $env:PATH" 
 
 copy .\tmp.dsp .\abclib.dsp
-del .\tmp.dsp
-$env:PATH = $CygwinBin
-& $Cygwin $arg "unix2dos ./*.dsp"
-$env:PATH = $OldPat
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+del tmp.dsp
+unix2dos .\*.dsp
 <# executing MsDevShell #>
-pwsh.exe -File .\function.ps1 -All
+pwsh.exe -File .\function.ps1
 
 devenv abcspace.dsw /upgrade  ; if (-not $? ) { cat UpgradeLog.htm }
 Write-Output "Build abc..."
